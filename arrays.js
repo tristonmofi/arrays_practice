16795cebdb70886ed6dabe7d0deee271d25e3476
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-03-29 10:14:22 -0600

    add array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,17 @@
 var friends = ["Elias", "Cody", "Bryer", "Kie"];
 var numbers = [1, 2, 3, 4]
 var boolean = [true, false, true, false]
+
+//the push method adds any number of elements following the last element in an array
+friends.push("Mitch", "Eddie");
+
+console.log(push);
+//the pop method removes the last element in an array
+numbers.pop();
+
+console.log(numbers);
+//the shift method will remove the first element of an array
+boolean.shift();
+
+console.log(boolean);
+
