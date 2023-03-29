bcbdd66c8a6a5ee34ac209a121cddafc175dad1b
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-03-29 10:21:18 -0600

    add explanations to index position

--- arrays.js
+++ arrays.js
@@ -6,12 +6,19 @@ var boolean = [true, false, true, false]
 friends.push("Mitch", "Eddie");
 
 console.log(push);
+
 //the pop method removes the last element in an array
 numbers.pop();
 
 console.log(numbers);
+
 //the shift method will remove the first element of an array
 boolean.shift();
 
 console.log(boolean);
 
+//In this array there are 4 elements; [1, 2, 3, 4]. Counting in an array starts with 0.
+//In this case 0 = 1, 1 = 2, 2 = 3, and 3 = 4. 
+//We do this to count more effeciently. If I were to tell you grab element 0 it would be 1 meaning you move 0 times.
+//If I ask you to grab element 3 you would grab 4 because you move over three times.
+var integers = [1, 2, 3, 4];
